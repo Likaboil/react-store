f8bbbe311254fc6044283ab35cffbd6c1a42fa9d
Liliya Sagitova <guverboil@gmail.com>
2021-02-19 18:33:11 +0300

    refactor(book-list): WIP Add lifecycle methos
    Add HOC to get data and fix default export.

--- src/components/pages/page-components/book-list/book-list.js
+++ src/components/pages/page-components/book-list/book-list.js
@@ -3,8 +3,18 @@ import { connect } from 'react-redux';
 import './book-list.css';
 
 import BookListItem from '../book-list-item';
+import {withBookstoreService} from '../../../hoc';
 
 class BookList  extends Component {
+
+  componentDidMount() {
+    // recieve data
+    const {bookstoreService} = this.props;
+    const data = bookstoreService.getBooks();
+
+    console.log(data);
+
+  }
 
   render() {
 
@@ -29,10 +39,9 @@ class BookList  extends Component {
 
 // describes what data the component will receive from redax
 const mapStateToProps = (state) => {
-
   return {
     books: state.books
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+export default withBookstoreService()(connect(mapStateToProps)(BookList));
