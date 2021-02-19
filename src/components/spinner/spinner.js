0cf4fb4eec214fce3400073a620c2d8a98150450
Liliya Sagitova <guverboil@gmail.com>
2021-02-19 14:22:38 +0300

    feat(spinner): Add comp Spinner
    Spinner will show loading status in app

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-hr9ng002mnn">
+      <div class="ldio-0hgklc7ncq9h">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
