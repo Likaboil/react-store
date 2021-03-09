44a0b72b7ca999ddd2cf5c5b916e74a8da051f05
Liliya Sagitova <guverboil@gmail.com>
2021-03-09 12:56:51 +0300

    refactor(reducer): Combine reducers with redux
    Remove unnecessary middkeware from store.
    Get one global state with two small ones inside

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,11 +1,8 @@
+import { combineReducers } from 'redux';
 import bookReducer from './books/books-reducer';
 import cartReducer from './cart';
 
-const reducer = (state, action) => {
-  return {
-    bookList: bookReducer(state, action),
-    cartList: cartReducer(state, action)
-  };
-};
-
-export default reducer;
+export default combineReducers({
+  bookList: bookReducer,
+  cartList: cartReducer
+});
